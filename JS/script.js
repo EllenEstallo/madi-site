82d024f16258afe2f70ddbb70f8e029fd3527384
Ellen Estallo <ellen.estallo@gmail.com>
2025-06-23 21:53:15 -0400

    started repertoire.html

--- JS/script.js
+++ JS/script.js
@@ -46,3 +46,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	observer.observe(textBlock);
 });
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion) => {
+	accordion.addEventListener("click", function () {
+		this.classList.toggle("active");
+		const panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
+});
